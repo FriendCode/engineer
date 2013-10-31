645520588ea54b2194323b0208361bd08349c5e2
Samy Pessé <samypesse@gmail.com>
2013-10-31 23:26:20 +0100

    First version

---
+++ example/main.js
@@ -0,0 +1,18 @@
+var engineer = require("../engineer");
+
+var app = new engineer.Application({
+    'paths': __dirname
+})
+
+app.load([
+    './test1',
+    {
+        packagePath: "./test2", port: 8080
+    }
+]).then(function() {
+    console.log("test chargement of test3:");
+    return app.load([
+        './test3'
+    ]);
+});
+
